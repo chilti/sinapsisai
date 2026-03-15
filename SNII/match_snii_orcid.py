170c24f0b8b6e0e4f63956d470602d2f9c65185e
chilti <chilti@gmail.com>
2026-03-15 10:25:46 -0600

    script sniis

--- SNII/match_snii_orcid.py
+++ SNII/match_snii_orcid.py
@@ -287,7 +287,7 @@ def get_orcid_emails(client, orcid):
         pass
     return ""
 
-def run_matching(limit=500, min_score=0.95):
+def run_matching(limit=500, min_score=0.75):
     client = get_client()
     graph_store = Neo4jGraphStore(uri=NEO4J_URI, user=NEO4J_USER, password=NEO4J_PASS)
     existing_mappings = load_existing_mappings()
